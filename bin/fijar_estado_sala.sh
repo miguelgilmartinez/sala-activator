9bd7f84221a30e3dce73f99b789b2ac03ca22ee0
Miguel Gil <miguelgilmartinez@gmail.com>
2025-04-22 21:43:18 +0200

    Cambios en scripts de snmp

--- bin/fijar_estado_sala.sh
+++ bin/fijar_estado_sala.sh
@@ -7,7 +7,7 @@ roclave="public"
 rwclave="carchuto"
 # Para pruebas, simulamos la ejecución del comando SNMP
 # En producción, descomenta la siguiente línea:
-# snmpset -v 2c -c $rwclave $swPlanta CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.101$i integer $vlan | sed 's/CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.101//' | awk '{print $1," ",$4}'
+snmpset -v 2c -c $rwclave $swPlanta CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.101$i integer $vlan | sed 's/CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.101//' | awk '{print $1," ",$4}'
 
 # En modo de prueba, simplemente devolvemos un resultado simulado
-echo "$swPlanta integer $vlan"
+#echo "$swPlanta integer $vlan"
