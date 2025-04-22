4abfa7f230110d26b7e2bd3e3b3a5b4877341bde
Miguel Gil <miguelgilmartinez@gmail.com>
2025-04-22 20:33:38 +0200

    feat(migration): Añadido fichero migration para la entidad SwitchSalas

--- migrations/Version20250421065112.php
+++ migrations/Version20250421065112.php
@@ -19,13 +19,19 @@ final class Version20250421065112 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE UNIQUE INDEX UNIQ_AD8F32DD3A909126 ON switch_salas (nombre)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE UNIQUE INDEX UNIQ_AD8F32DDA5E3B32D ON switch_salas (ip)
-        SQL);
+        $this->addSql("CREATE TABLE switch_salas (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    ip VARCHAR(15) NOT NULL,
+    CONSTRAINT uk_switch_salas_nombre UNIQUE (nombre),
+    CONSTRAINT uk_switch_salas_ip UNIQUE (ip));");
+//        // this up() migration is auto-generated, please modify it to your needs
+//        $this->addSql(<<<'SQL'
+//            CREATE UNIQUE INDEX UNIQ_AD8F32DD3A909126 ON switch_salas (nombre)
+//        SQL);
+//        $this->addSql(<<<'SQL'
+//            CREATE UNIQUE INDEX UNIQ_AD8F32DDA5E3B32D ON switch_salas (ip)
+//        SQL);
     }
 
     public function down(Schema $schema): void
