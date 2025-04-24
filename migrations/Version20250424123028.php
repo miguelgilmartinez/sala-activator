6567a316c2a68c08f4b5a4c54c56e01ec381a870
Miguel Gil Martínez <miguel.gil.martinez@juntadeandalucia.es>
2025-04-24 14:31:08 +0200

    feat(swtichsalas): Añadido soporte para configuración de vlan y puertos

---
+++ migrations/Version20250424123028.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250424123028 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE switch_salas ADD vlan VARCHAR(255) DEFAULT 'FAKEVLAN' NOT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE switch_salas ADD puertos VARCHAR(255) DEFAULT 'FAKEPUERTO1 FAKEPUERTO2' NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE SCHEMA public
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE switch_salas DROP vlan
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE switch_salas DROP puertos
+        SQL);
+    }
+}
--- src/Entity/SwitchSalas.php
+++ src/Entity/SwitchSalas.php
@@ -19,6 +19,12 @@ class SwitchSalas {
     #[ORM\Column(length: 15, unique: true)]
     private ?string $ip = null;
 
+    #[ORM\Column(length: 255, options: ['default' => 'FAKEVLAN'])]
+    private ?string $vlan = 'FAKEVLAN';
+
+    #[ORM\Column(length: 255, options: ['default' => 'FAKEPUERTO1 FAKEPUERTO2'])]
+    private ?string $puertos = 'FAKEPUERTO1 FAKEPUERTO2';
+
     public function getId(): ?int {
         return $this->id;
     }
@@ -39,6 +45,26 @@ class SwitchSalas {
 
     public function setIp(string $ip): static {
         $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getVlan(): ?string {
+        return $this->vlan;
+    }
+
+    public function setVlan(string $vlan): static {
+        $this->vlan = $vlan;
+
+        return $this;
+    }
+
+    public function getPuertos(): ?string {
+        return $this->puertos;
+    }
+
+    public function setPuertos(string $puertos): static {
+        $this->puertos = $puertos;
 
         return $this;
     }
