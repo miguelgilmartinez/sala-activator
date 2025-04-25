e2f100371df1b17b3c12a845a729067cefca65d8
Miguel Gil Martínez <miguel.gil.martinez@juntadeandalucia.es>
2025-04-25 14:20:09 +0200

    Más código

--- bin/leer_estado_salas.sh
+++ bin/leer_estado_salas.sh
@@ -4,4 +4,4 @@
 swPlanta=$1 #PROD
 roclave="public"
 echo "PUERTA VLAN ($swPlanta)" #DEV
-snmpwalk -v 2c -c $roclave $swPlanta CISCO-VLAN-MEMBERSHIP-MIB::vmVlan | sed 's/CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.10[61]//' | awk '{print $1," ",$4}' #PROD
+snmpwalk -v 2c -c $roclave $swPlanta CISCO-VLAN-MEMBERSHIP-MIB::vmVlan | sed 's/CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.10[61]//' | awk '{print $4," ",$1}' #PROD
