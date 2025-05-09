98dc8e0e1542fda9d96be534febc72f957ea76ac
Miguel Gil Martínez <miguel.gil.martinez@juntadeandalucia.es>
2025-05-09 13:07:32 +0200

    Perdido el fichero .env.dev

--- bin/fijar_estado_sala.sh
+++ bin/fijar_estado_sala.sh
@@ -12,8 +12,10 @@ rwclave="carchuto"
 # En producción, descomenta la siguiente línea:
 #snmpset -v 2c -c $rwclave $swPlanta CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.101$i integer $vlan | sed 's/CISCO-VLAN-MEMBERSHIP-MIB::vmVlan.101//' | awk '{print $1," ",$4}' >>  /tmp/salas.log
 
-for 
-echo snmpset -v 2c -c $rwclave $swPlanta  .1.3.6.1.4.1.9.9.68.1.2.2.1.2.101$i integer $vlan
+for i in $*
+    do
+        echo snmpset -v 2c -c $rwclave $swPlanta  .1.3.6.1.4.1.9.9.68.1.2.2.1.2.101$i integer $vlan
+    done
 
 # En modo de prueba, simplemente devolvemos un resultado simulado
 #echo "$swPlanta integer $vlan"
